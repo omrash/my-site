14580e4fd0823e64727182bdc1207e37c3d455b9
Omar Rashwan <omar@ccs.neu.edu>
2026-01-24 12:50:47 -0500

    Add missing HTML and body tags to layout

--- app/layout.js
+++ app/layout.js
@@ -4,5 +4,11 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
-  return children
+  return (
+    <html>
+      <body>
+        {children}
+      </body>
+    </html>
+  )
 }
